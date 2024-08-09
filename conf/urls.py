65cf723bee057590d38b3da01d6b62d798135b00
farhadshakiba1988 <farhadshakiba1988@gmail.com>
2024-08-09 19:23:40 +0330

    add init for models packeage and add views folder

--- conf/urls.py
+++ conf/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from conf import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.farhad, name='farhad'),
     path('report_all/', views.report_all, name='report_all'),
     path('bazarghani_report/', views.bazarghani_report, name='bazarghani_report'),
 ]
