dc9777427f27ff9744607bdfebffd3ca6953971b
farhadshakiba1988 <farhadshakiba1988@gmail.com>
2024-08-09 23:54:45 +0330

    change invoice tempalte

--- conf/urls.py
+++ conf/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from .Views.bazarghani_report_views import LeadInfo
 from .Views.index_views import Farhad
+from .Views.invoice_views import Invoice
 from .Views.report_all_views import Lead
-from conf import views
 
 app_name = 'conf'
 urlpatterns = [
     path('', Farhad.as_view(), name='farhad'),
+    path('invoice/', Invoice.as_view(), name='invoice'),
     path('report_all/', Lead.as_view(), name='report_all'),
     path('bazarghani_report/', LeadInfo.as_view(),
          name='bazarghani_report'),
